da4c09ea39f2b9ee2a4a41e2e565404d5be27886
remorses <beats.by.morse@gmail.com>
2025-02-23 12:13:29 +0100

    created shotcut example

--- scripts/melt.ts
+++ scripts/melt.ts
@@ -2,7 +2,7 @@ import { spawnSync } from 'child_process'
 
 try {
     const output = spawnSync(
-        '/Applications/kdenlive.app/Contents/MacOS/melt sololevelling/page-000.jpg in=:0.0 out=:5.0 -consumer xml:test.mlt',
+        '/Applications/kdenlive.app/Contents/MacOS/melt "sololevelling/page-000.jpg" in=:0.0 out=:5.0 "/Volumes/1tb sabrent/Screens/Screen Recording 2024-12-13 at 10.50.31.mov" -consumer xml:test.mlt',
         {
             shell: true,
             stdio: 'inherit',
